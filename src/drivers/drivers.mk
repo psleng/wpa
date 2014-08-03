b908dea8d73d6abf8068db1b175445b6499f653d
Kim Hagen <khagen@multi-development.com>
2014-08-03 17:56:50 +0200

    Add libnl-3 to CFLAGS

--- src/drivers/drivers.mk
+++ src/drivers/drivers.mk
@@ -48,7 +48,7 @@ NEED_RFKILL=y
 ifdef CONFIG_LIBNL32
   DRV_LIBS += -lnl-3
   DRV_LIBS += -lnl-genl-3
-  DRV_CFLAGS += -DCONFIG_LIBNL20
+  DRV_CFLAGS += -DCONFIG_LIBNL20 `pkg-config --cflags libnl-3.0`
 else
   ifdef CONFIG_LIBNL_TINY
     DRV_LIBS += -lnl-tiny
